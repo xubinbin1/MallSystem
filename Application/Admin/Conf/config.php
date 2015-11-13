e0aa0d734361d1babdc7bf8804c8afb4505b480b
xubinbin <xubinbin10578@sina.com>
2015-11-13 22:57:18 +0800

    display login

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	/* 模板相关配置 */
+    'TMPL_PARSE_STRING' => array(
+        '__STATIC__' => __ROOT__ . '/Public/Static',
+        '__IMG__'    => __ROOT__ . '/Public/' . MODULE_NAME . '/img',
+        '__CSS__'    => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
+        '__JS__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
+    )
 );
